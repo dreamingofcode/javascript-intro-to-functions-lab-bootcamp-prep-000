d07d2837434f5a1cdf62f1fd938eff5f9653a2da
cesar mota <cesarmota@ymail.com>
2019-01-16 20:36:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string)
+{
 var uppercase = "HELLO!"
  
 uppercase.toUpperCase() === uppercase // true
@@ -32,7 +33,7 @@ var mixedCase = 'Hi there!'
  
 mixedCase.toLowerCase() === mixedCase // false
  
-mixedCase.toUpperCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase // false}
 {if (string===lowercase){console.log("I can\'t hear you!")}
 else if(string===uppercase){console.log("YES INDEED!")}
   else {console.log("I love you, Grandma.")
