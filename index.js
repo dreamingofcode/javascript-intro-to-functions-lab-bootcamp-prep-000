097f8d80b8edfc761ff9219f928f9147f8a31242
cesar mota <cesarmota@ymail.com>
2019-01-16 20:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,7 @@ var lowercase = 'hello!'
  
 lowercase.toLowerCase() === lowercase // true
  
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false}
+}
 {if (string===lowercase){console.log("I can\'t hear you!")}
 else if(string===uppercase){console.log("YES INDEED!")}
   else {console.log("I love you, Grandma.")
