3af033a8c152f8ba68c59f3cc3c84cf1c3d967df
cesar mota <cesarmota@ymail.com>
2019-01-16 20:21:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{string.toUpperCase
+return string.toLowerCase()}
+
+
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+var uppercase="HELLO!"
+var lowercase="hello!"
+
+function sayHiToGrandma(string)
+{if (string===lowercase){console.log("I can't hear you!")}
+else if(string===uppercase){console.log("YES INDEED!")}
+  else {console.log("I love you, Grandma.")
+}}
